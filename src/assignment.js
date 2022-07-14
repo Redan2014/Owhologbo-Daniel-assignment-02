1c818d2549de9102ac161c88e404937315dd3f95
Owhologbo P.O. Daniel <princedo22002@gmail.com>
2022-07-14 16:25:26 +0000

    Attempt Challenge - 1 and -2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers (arrayOfNumbers) {
+  let sumOfNumbers = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i += 1) {
+    sumOfNumbers += arrayOfNumbers [i];
+  }
+     return sumOfNumbers;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +33,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countOfEvenNumbers = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i =+ 1){
+    if (arrayOfNumbers [i] % 2 === 0) {
+      countOfEvenNumbers += 1;
+    } else {
+      countEvenNumbers += 0;
+    }
+  }
+    return countOfEvenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
